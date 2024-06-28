18723a989968953c262e1b70a38594bc0c5cc494
natalialukash <lukashova.n13@gmail.com>
2024-06-28 19:01:15 +0300

    шрифты

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header/Header";
 import Banner from "./components/Banner/Banner";
-import "../public/assets/fonts/Montserrat_Alternates/MontserratAlternates-Medium.ttf";
+import "/assets/fonts/Montserrat_Alternates/MontserratAlternates-Medium.ttf";
 import ProgrammsList from "./components/Programms/Programms";
 import AboutUs from "./components/AboutUsBlock/AboutUsBlock";
 import TeachersBlock from "./components/TeachersBlock/TeachersBlock";
