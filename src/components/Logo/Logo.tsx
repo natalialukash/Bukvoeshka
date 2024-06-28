112f20b2035604fba4e7f41b01ccbe94e04eef11
natalialukash <lukashova.n13@gmail.com>
2024-06-28 18:34:45 +0300

    .

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,7 +5,7 @@ const Logo = () => {
     <>
       <div className={classes.containerLogo}>
         <div>Букво</div>
-        <img src={"../../../public/assets/icons/letter_e_logo.svg"} alt="Logo"/>
+        <img src={"../assets/icons/letter_e_logo.svg"} alt="Logo" />
         <div>шка</div>
       </div>
     </>
